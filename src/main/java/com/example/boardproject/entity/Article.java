c1e8f654a281edac588c82249cd0137db3c2fc63
asus <kkw-11@naver.com>
2022-07-09 22:26:30 +0900

    refactor: Article Entity, Lombok AllArgsConstructor, ToString

--- src/main/java/com/example/boardproject/entity/Article.java
+++ src/main/java/com/example/boardproject/entity/Article.java
@@ -1,11 +1,16 @@
 package com.example.boardproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity //DB가 해당객체를 인식 하게 해줌
+@AllArgsConstructor
+@ToString
 public class Article {
     @Id
     @GeneratedValue //ID값이 1,2,3 자동 생성하게 해주는 기능
@@ -17,18 +22,4 @@ public class Article {
     private String content;
 
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
