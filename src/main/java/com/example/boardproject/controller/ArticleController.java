5834ad469a443d07ba22976a527f0dc7ded2f4ee
asus <kkw-11@naver.com>
2022-07-09 23:51:57 +0900

    feature: ArticleController showArticle method, ArticleEntity defalut constructor

--- src/main/java/com/example/boardproject/controller/ArticleController.java
+++ src/main/java/com/example/boardproject/controller/ArticleController.java
@@ -6,7 +6,9 @@ import com.example.boardproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @Slf4j // Loggin Annotation
@@ -24,7 +26,6 @@ public class ArticleController {
 //        System.out.println(form.toString()); //-> Loggin replacing
         log.info(form.toString());
 
-
         //1. DTO를 Entity로 변환
         Article article = form.toEntity();
 //        System.out.println(article.toString());
@@ -36,5 +37,19 @@ public class ArticleController {
         log.info(savedArticle.toString());
 
         return "redirect:/";
+    }
+
+
+    @GetMapping("/articles/{id}")
+    public String showArticle(@PathVariable Long id, Model model){
+        log.info("id = " + id);
+        // 1: id로 데이터를 가져옴
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2: 가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        // 3: 보여줄 페이지 설정
+        return "articles/showArticle";
     }
 }
--- src/main/java/com/example/boardproject/entity/Article.java
+++ src/main/java/com/example/boardproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.boardproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 @Entity //DB가 해당객체를 인식 하게 해줌
 @AllArgsConstructor
 @ToString
+@NoArgsConstructor // defalut constructor
 public class Article {
     @Id
     @GeneratedValue //ID값이 1,2,3 자동 생성하게 해주는 기능
