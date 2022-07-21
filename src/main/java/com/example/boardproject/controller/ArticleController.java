8108d6374f14c377911cb15d9983a2140add623c
asus <kkw-11@naver.com>
2022-07-22 00:38:58 +0900

    feature: delete controller add

--- src/main/java/com/example/boardproject/controller/ArticleController.java
+++ src/main/java/com/example/boardproject/controller/ArticleController.java
@@ -4,12 +4,14 @@ import com.example.boardproject.dto.ArticleForm;
 import com.example.boardproject.entity.Article;
 import com.example.boardproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.apache.bcel.util.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.swing.text.html.Option;
 import java.util.ArrayList;
@@ -96,5 +98,25 @@ public class ArticleController {
         
         // 3: 수정 결과 페이지로 리다이렉트 한다.
         return "redirect:/articles/" + articleEntity.getId();
+    }
+
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제 요청이 들어왔습니다!");
+        //1. DB에서 삭제 대상을 찾는다.
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        //2. 대상을 삭제한다!
+        if(target != null){
+            articleRepository.delete(target);
+        }
+
+        //3. 결과 페이지로 리다이렉트 한다!
+        rttr.addFlashAttribute("msg","삭제가 완료되었습니다!");
+
+
+        return "redirect:/articles";
     }
 }
